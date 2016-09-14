f3b83c558ac9b00ff52a60c75014daccb030d57b
willardshikami <willardshikami@gmail.com>
2016-09-14 10:42:04 +0300

    added method to create new user on firebase

--- app/src/main/java/com/example/willardpc/firebaseauthdemo/MainActivity.java
+++ app/src/main/java/com/example/willardpc/firebaseauthdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.willardpc.firebaseauthdemo;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
@@ -10,6 +11,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -70,7 +74,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         progressDialog.setMessage("Registering new user...");
         progressDialog.show();
 
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
 
+            }
+        })
 
     }
 

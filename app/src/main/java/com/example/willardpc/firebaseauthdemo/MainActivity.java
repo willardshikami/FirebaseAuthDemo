ca1d7e25b79d212f1a62e061e17eb8dd702d3a2d
willardshikami <willardshikami@gmail.com>
2016-09-14 10:33:02 +0300

    added and initialized firebase auth object

--- app/src/main/java/com/example/willardpc/firebaseauthdemo/MainActivity.java
+++ app/src/main/java/com/example/willardpc/firebaseauthdemo/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.willardpc.firebaseauthdemo;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -15,11 +20,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText editTextPassword;
     private TextView textViewSignin;
 
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
 
         buttonRegister = (Button) findViewById(R.id.buttonRegister);
 
@@ -33,8 +46,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textViewSignin.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
+    //
+    private void registerUser(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
 
+        if (TextUtils.isEmpty(email)){
+            //EMPTY EMAIL.
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            //STOPS THE FUNCTION FROM EXECUTING FURTHER
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)){
+            //EMPTY PASSWORD
+            Toast.makeText(this, "Please enter Password", Toast.LENGTH_SHORT).show();
+            //STOPS THE FUNCTION FROM EXECUTING FURTHER
+            return;
+        }
+        //IF VALIDATIONS ARE OK
+        //SHOW THE PROGRESSBAR
+
+        progressDialog.setMessage("Registering new user...");
+        progressDialog.show();
+
+
+
+    }
+
+    //
+    @Override
+    public void onClick(View view) {
+        if (view == buttonRegister){
+            registerUser();
+        }
+
+        if (view == textViewSignin){
+            //WILL OPEN LOGIN ACTIVITY HERE
+        }
     }
 }
